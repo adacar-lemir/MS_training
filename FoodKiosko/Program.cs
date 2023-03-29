9f9969e63162e4c319ccd98da74d158c5cb30d0b
Adacar Lemir <adacar.lemir@gmail.com>
2023-03-29 14:40:40 -0300

    swagger addition

--- FoodKiosko/Program.cs
+++ FoodKiosko/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.OpenApi.Models; // dotnet add package Swashbuckle.AspNetCore --version 6.1.4
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options => { });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => 
+{ 
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "FoodKiosko", Description = "A Kiosko of food", Version = "v1" });
+});
 
 var app = builder.Build();
 
 app.UseCors("some unique string");
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "FoodKiosko API V1");
+});
 
 app.Run();
