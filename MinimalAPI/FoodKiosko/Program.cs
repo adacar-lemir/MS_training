09a93d788a30ddedcafb26980f6c6c94a9d2e9db
Adacar Lemir <adacar.lemir@gmail.com>
2023-03-30 16:26:57 -0300

    added extra CRUD endpoints with EF In-memory usage

--- MinimalAPI/FoodKiosko/Program.cs
+++ MinimalAPI/FoodKiosko/Program.cs
@@ -19,8 +19,34 @@ var app = builder.Build();
 
 app.UseCors("some unique string");
 
-app.MapGet("/", () => "Welcoming to FoodKiosko!");
 app.MapGet("/foods", async (FoodDb db) => await db.Foods.ToListAsync());
+app.MapGet("/food/{id}", async (FoodDb db, int id) => await db.Foods.FindAsync(id));
+app.MapPost("/food", async (FoodDb db, Food food) =>
+{
+	await db.Foods.AddAsync(food);
+	await db.SaveChangesAsync();
+	return Results.Created($"/food/{food.Id}", food);
+});
+app.MapPut("/food/{id}", async (FoodDb db, Food updateFood, int id) => 
+{
+	var food = await db.Foods.FindAsync(id);
+	if (food is null) return Results.NotFound();
+	food.Name = updateFood.Name;
+	food.Description = updateFood.Description;
+	await db.SaveChangesAsync();
+	return Results.NoContent();
+});
+app.MapDelete("/food/{id}", async (FoodDb db, int id) =>
+{
+	var food = await db.Foods.FindAsync(id);
+	if (food is null)
+	{
+		return Results.NotFound();
+	}
+	db.Foods.Remove(food);
+	await db.SaveChangesAsync();
+	return Results.Ok();
+});
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
