9f1fc775f4fc5102c4b69a834a58075ba3900da3
Adacar Lemir <adacar.lemir@gmail.com>
2023-03-30 17:14:25 -0300

    using SQLite with EF Core

---
+++ MinimalAPI/FoodKiosko/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodKiosko.Models
+{
+	public class Food
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+	}
+
+	// dotnet add package Microsoft.EntityFrameworkCore.InMemory --version 6.0
+	class FoodDb :DbContext
+	{
+		public FoodDb(DbContextOptions options) : base(options) { }
+		public DbSet<Food> Foods { get; set; } = null!;
+	}
+}
