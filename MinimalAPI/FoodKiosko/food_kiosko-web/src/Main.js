a71ec9eb7fa52a3c5efa402edde834177a415141
Adacar Lemir <adacar.lemir@gmail.com>
2023-03-31 18:43:45 -0300

    added some css and related configs

--- MinimalAPI/FoodKiosko/food_kiosko-web/src/Main.js
+++ MinimalAPI/FoodKiosko/food_kiosko-web/src/Main.js
@@ -2,9 +2,40 @@
 // npx create-react-app food_kiosko-web
 //
 // create this file
+// yarn add styled-components
 
 
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const FoodFrame = styled.div`
+    border: solid 1px gray;
+    padding: 10px;
+    margin: 15px 10px;
+    border-radius: 5px;
+    box-shadow: 0 0 5px grey;
+    font-family: Arial;
+`;
+
+const Input = styled.input`
+    border: solid 1px black;
+    padding: 5px;
+    border-radius: 3px;
+`;
+
+const Title = styled(Input)`
+    text-transform: uppercase;
+`;
+
+const Save = styled.button`
+   width: 100px;
+   margin: 10px;
+   background: green;
+   color: white;
+   font-size: 16px;
+   padding: 10px;
+   border-radius: 5px;
+`;
 
 let foods = [{
 		id: 1, name: 'SpagguItaly', description: 'Italian fideos'
@@ -12,7 +43,7 @@ let foods = [{
 	{
 		id: 2, name: 'Jamburger', description: 'Sweet meat with breads'
 	}
-}];
+];
 
 const Food = ({ food }) => {
 	const [data, setData] = useState(food);
@@ -29,17 +60,17 @@ const Food = ({ food }) => {
 	}
 
 	return (<React.Fragment>
-		<div>
+		<FoodFrame>
 			<h3>
-				<input onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} />
+				<Title onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} />
 			</h3>
-		</div>
-		<div>
-			<input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
-		</div>
-		{dirty ?
-			<div><button onClick={onSave}>Save</button></div> : null
-		}
+			<div>
+				<Input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
+			</div>
+			{dirty ?
+				<div><Save onClick={onSave}>Save</Save></div> : null
+			}
+		</FoodFrame>
 	</React.Fragment>);
 }
 
